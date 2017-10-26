99a6b5ca7dfd078ea8656b5db2b684be451587aa
Niclas Valfridsson <waffe91@gmail.com>
2017-10-26 13:05:12 +0200

    Forgot to press save

--- FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer.Model/Friend.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace FriendOrganizer.Model
 {
-    public class Class1
+    public class Friend
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
