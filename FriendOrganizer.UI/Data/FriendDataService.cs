9a9c35d8bdb4ed8cbecc79f1dc7c64fe23d8f654
Niclas Valfridsson <waffe91@gmail.com>
2017-10-26 15:16:42 +0200

    Added viewmodel, view, autofac.

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    class FriendDataService : IFriendDataService
+    {      
+        public IEnumerable<Friend> GetAll()
+        {
+            // TODO: Load data from a real database
+            yield return new Friend { FirstName = "Daniel", LastName = "Andersson" };
+            yield return new Friend { FirstName = "Fredik", LastName = "Fandersson" };
+            yield return new Friend { FirstName = "Huber", LastName = "Carlsson" };
+            yield return new Friend { FirstName = "Juber", LastName = "Andersson" };
+        }
+    }
+}
---
+++ FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MainViewModel:ViewModelBase
+    {
+        private IFriendDataService _friendDataService;
+
+
+        public MainViewModel(IFriendDataService friendDataService)
+        {
+            Friends = new ObservableCollection<Friend>();
+            _friendDataService = friendDataService;
+        }
+
+        public void Load()
+        {
+            var friends = _friendDataService.GetAll();
+            Friends.Clear();
+            foreach (var friend in friends)
+            {
+                Friends.Add(friend);
+            }
+        }
+
+        public ObservableCollection<Friend> Friends { get; set; }
+        private Friend _selectedFriend;
+
+        public Friend SelectedFriend
+        {
+            get { return _selectedFriend; }
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+
+
+    }
+}
